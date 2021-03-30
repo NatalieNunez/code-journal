d8134672f5fdcb108403b0277366bd25fc5ec767
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-30 15:38:20 -0700

    Add the nextEntryId to the object.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,35 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var $form = document.querySelector('form');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var values;
+  values = event.target.elements;
+  // console.log('entries', entries);
+
+  var array = [];
+  for (var i = 0; i < 3; i++) {
+    array.push([values[i].name, values[i].value]);
+    // console.log('array:', array);
+  }
+
+  var object = {};
+  for (var k = 0; k < array.length; k++) {
+    object[array[k][0]] = array[k][1];
+  }
+
+  for (var key in data) {
+    if (key === 'nextEntryId') {
+      object[key] = data[key];
+    }
+  }
+  // console.log('object value after:', object);
+  // var jsonStr = JSON.stringify(object);
+  // localStorage.setItem('entries', jsonStr);
+  // console.log('object', object);
+}
+
+$form.addEventListener('submit', handleSubmit);
--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 /* exported data */
 var $photoUrl = document.querySelector('#imageUrl');
 var $photo = document.querySelector('img');
-var $form = document.querySelector('form');
 
 function handleInput(event) {
   var userImageUrl = event.target.value;
@@ -10,24 +9,3 @@ function handleInput(event) {
 }
 
 $photoUrl.addEventListener('input', handleInput);
-
-function handleSubmit(event) {
-  event.preventDefault();
-  var entries;
-  entries = event.target.elements;
-  // console.log('entries', entries);
-
-  var array = [];
-  for (var i = 0; i < 3; i++) {
-    array.push([entries[i].name, entries[i].value]);
-    // console.log('array:', array);
-  }
-
-  var object = {};
-  for (var k = 0; k < array.length; k++) {
-    object[array[k][0]] = array[k][1];
-  }
-  // console.log('object', object);
-}
-
-$form.addEventListener('submit', handleSubmit);
