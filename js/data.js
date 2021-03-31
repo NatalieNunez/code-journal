6fe511d6e2509307fcc9a2df4bb920ffd9b5d01e
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-03-30 19:35:49 -0700

    Verify that new entries are saved to local storage along with the rest of the data model on page reload.

--- js/data.js
+++ js/data.js
@@ -7,42 +7,14 @@ var data = {
   nextEntryId: 1
 };
 
-var $form = document.querySelector('form');
-var $photo = document.querySelector('img');
-
-function handleSubmit(event) {
-  event.preventDefault();
-  var values;
-  values = event.target.elements;
-  // console.log('entries', entries);
-
-  var array = [];
-  for (var i = 0; i < 3; i++) {
-    array.push([values[i].name, values[i].value]);
-    // console.log('array:', array);
-  }
-
-  var object = {};
-  for (var k = 0; k < array.length; k++) {
-    object[array[k][0]] = array[k][1];
-  }
-
-  for (var key in data) {
-    if (key === 'nextEntryId') {
-      object[key] = data[key];
-    }
-  }
-
-  data.nextEntryId = data.nextEntryId + 1;
-  data.entries.unshift(object);
-  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $form.reset();
-  // console.log('object value:', object);
-  // console.log(data);
-
-  // var jsonStr = JSON.stringify(object);
-  // localStorage.setItem('entries', jsonStr);
-  // console.log('object', object);
+var previousEntriesJSON = localStorage.getItem('entry-data');
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
 }
 
-$form.addEventListener('submit', handleSubmit);
+function beforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('entry-data', dataJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 /* exported data */
 var $photoUrl = document.querySelector('#imageUrl');
 var $photo = document.querySelector('img');
+var $form = document.querySelector('form');
 
 function handleInput(event) {
   var userImageUrl = event.target.value;
@@ -9,3 +10,35 @@ function handleInput(event) {
 }
 
 $photoUrl.addEventListener('input', handleInput);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var values;
+  values = event.target.elements;
+
+  var array = [];
+  for (var i = 0; i < 3; i++) {
+    array.push([values[i].name, values[i].value]);
+  }
+
+  var object = {};
+  for (var k = 0; k < array.length; k++) {
+    object[array[k][0]] = array[k][1];
+  }
+
+  for (var key in data) {
+    if (key === 'nextEntryId') {
+      object[key] = data[key];
+    }
+  }
+
+  data.nextEntryId = data.nextEntryId + 1;
+  data.entries.unshift(object);
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+
+  // console.log('object value:', object);
+  // console.log('data', data);
+}
+
+$form.addEventListener('submit', handleSubmit);
